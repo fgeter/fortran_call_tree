81b33000956eec41307c0095a8278b5b4e3c51ab
Frank Geter <fgeter@gmail.com>
2025-02-14 17:00:12 -0700

    1st code with .dot file output but just for subroutines called by main.f90

--- fctree.py
+++ fctree.py
@@ -30,7 +30,9 @@ if len(sys.argv) == 1:
 else:
     arg = sys.argv[1]
     arg = os.path.join(arg, "*.f90")
-# for arg in sys.argv[1:]:
+
+
+# The first part of the code collects the names of subroutines, functions, and modules
 subroutine_names = []
 function_names = []
 module_names = []
@@ -40,16 +42,16 @@ for file in glob.iglob(arg):
     # opath = "/".join([output_folder, ofile])
     with open(file, errors='ignore') as fp:
         # with open(opath, "w") as ofp:
-        pf = True
-        line_num = 0
+        # pf = True
+        # line_num = 0
         line = fp.readline()
-        intent_vars = []
+        # intent_vars = []
         # function_names = []
-        function_args = []
+        # function_args = []
         # subroutine_names = []
-        subroutine_args = []
-        external_func = []
-        skip_vars = []
+        # subroutine_args = []
+        # external_func = []
+        # skip_vars = []
         while line:
             exe_portion = line.split("!")[0].strip().lower()
             exe_portion_list = re.split(r"\W+", exe_portion.strip())
@@ -72,8 +74,8 @@ for file in glob.iglob(arg):
             # if "function" in line.split("!")[0]:
             if len(exe_portion_list) > 0:
                 if "function" == exe_portion_list[0]:
-                    function_names.append(line.split("!")[0].split()[1].split("(")[0].strip())
-                    function_names.append(exe_portion_list[1])
+                    # function_names.append(line.split("!")[0].split()[1].split("(")[0].strip())
+                    function_names.append((exe_portion_list[1], file))
                     # function_args = line.split("(")[1].split(")")[0].split(",")
                     # function_args = [i.strip() for i in function_args]
                     # ofp.write(line)
@@ -82,7 +84,7 @@ for file in glob.iglob(arg):
                 # if "subroutine" in line.split("!")[0]:
                 if "subroutine" == exe_portion_list[0]:
                     # subroutine_names.append(line.split("!")[0].split()[1].split("(")[0].strip())
-                    subroutine_names.append(exe_portion_list[1])
+                    subroutine_names.append((exe_portion_list[1],file))
                     # if len(line.split("(")) > 1:
                     #     subroutine_args = line.split("(")[1].split(")")[0].split(",")
                     #     subroutine_args = [i.strip() for i in subroutine_args]
@@ -92,11 +94,11 @@ for file in glob.iglob(arg):
                 # if "module" == line.split("!")[0].split()[0].strip().lower():
                 if "module" == exe_portion_list[0]:
                     # module_names.append(line.split("!")[0].split()[0].strip().lower())
-                    module_names.append(exe_portion_list[1])
+                    module_names.append((exe_portion_list[1], file))
                     # module_names.append(line.split("!")[0].split()[1].split("(")[0].strip())
-                    if len(line.split("(")) > 1:
-                        subroutine_args = line.split("(")[1].split(")")[0].split(",")
-                        subroutine_args = [i.strip() for i in subroutine_args]
+                    # if len(line.split("(")) > 1:
+                    #     subroutine_args = line.split("(")[1].split(")")[0].split(",")
+                    #     subroutine_args = [i.strip() for i in subroutine_args]
                     # ofp.write(line)
                     # line = fp.readline()
                     # continue
@@ -114,3 +116,43 @@ print()
 print(module_names)
 print()
 print()
+
+# Second part of the code creates a dictionay representing the call tree.
+# This dictionary will later be converted to .dot graph file.  
+# The dictionary structure is similar to the hierarchy of .dot file. 
+# The starting point is main.f90 in source path.
+
+def recurse_code(file, dot_string):
+    # global subroutine_names
+    # global function_names
+    with open(file, errors='ignore') as fp:
+        # first make a pass through the file to collect a subroutine calls
+        calls = []
+        for line in fp.readlines():
+            exe_portion = line.split("!")[0].strip().lower()
+            exe_portion_list = re.split(r"\W+", exe_portion.strip())
+            if "call" == exe_portion_list[0]:
+                calls.append(exe_portion_list[1])
+        # add calls to dot_string
+        i = 0
+        for call in calls:
+            i += 1
+            if i == 1:
+                dot_string += "{"
+            dot_string += " " + call + " "
+        dot_string += "}\n"
+    return dot_string
+
+
+dot_string = "graph {\nrankdir=LR\nmain -- "
+dot_string = recurse_code("src/main.f90", dot_string)
+dot_string += "}"
+print(dot_string)
+with open("swatplus_call_tree.dot", "w") as wp:
+    wp.write(dot_string)
+                
+
+
+
+
+ 
