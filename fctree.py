2bb16edfb77bbde36fb40cd9ea9f49e3d4c29567
Frank Geter <fgeter@gmail.com>
2025-02-15 13:59:02 -0700

    Improved version of fctree.py that eliminates loop backs.

--- fctree.py
+++ fctree.py
@@ -4,153 +4,98 @@ import glob
 import os
 from func_pars_vars import *
 from func_parse_allocate import *
+from collections import OrderedDict
 
-exclusions = ["parameter"]
-integer_allocatable_vars = set()
-integer_allocatable_vars_with_save = set()
-real_allocatable_vars = set()
-real_allocatable_vars_with_save = set()
-character_allocatable_vars = set()
-character_allocatable_vars_with_save = set()
-allocatable_types = set()
-allocatable_types_with_save = set()
-fortran_intrinsics = ["abs", "sqrt", "sin", "sinh", "cos", "cosh", "tan", "tanh", \
-                      "asin", "acos", "atan", "atan2", "exp", "log", "log10", \
-                      "int", "nint", "floor", "sqrt", "fraction", \
-                      "max", "min", "mod", "modulo", "nint", "sign"]
-
-output_folder = "./src_stage1"
-try:
-    os.makedirs(output_folder, exist_ok = True)
-except OSError as error:
-    print(f"Directory {output_folder} could not be created.")
-    print(error)
 if len(sys.argv) == 1:
     arg = "src/*.f90"
 else:
     arg = sys.argv[1]
     arg = os.path.join(arg, "*.f90")
 
-
 # The first part of the code collects the names of subroutines, functions, and modules
-subroutine_names = []
+subroutine_names = {}
 function_names = []
 module_names = []
+sub_tokens_beg = ["subroutine", "program"]
+sub_tokens_end = ["endsubroutine", "end subroutine", "endprogram", "end program"]
 for file in glob.iglob(arg):
     print("Processing file :",file)
-    # ofile = file.split("/")[1]
-    # opath = "/".join([output_folder, ofile])
     with open(file, errors='ignore') as fp:
-        # with open(opath, "w") as ofp:
-        # pf = True
-        # line_num = 0
         line = fp.readline()
-        # intent_vars = []
-        # function_names = []
-        # function_args = []
-        # subroutine_names = []
-        # subroutine_args = []
-        # external_func = []
-        # skip_vars = []
+        in_sub = False
         while line:
             exe_portion = line.split("!")[0].strip().lower()
             exe_portion_list = re.split(r"\W+", exe_portion.strip())
             while '' in exe_portion_list:   # This necessary because sometimes there is a trailing '' in list
                 exe_portion_list.remove('')
-            # ignore_line = False
-            # for v in ["function", "subroutine"]:
-            #     if any(x == v for x in exe_portion_list):
-            #         ignore_line = False
-            #         continue
-            #     else:
-            #         for token in exclusions:
-            #             if any(x == token for x in exe_portion_list):
-            #                 ignore_line = True
-            # if ignore_line:
-            #     ofp.write(line)
-            #     line = fp.readline()
-            #     continue
-
-            # if "function" in line.split("!")[0]:
             if len(exe_portion_list) > 0:
-                if "function" == exe_portion_list[0]:
-                    # function_names.append(line.split("!")[0].split()[1].split("(")[0].strip())
-                    function_names.append((exe_portion_list[1], file))
-                    # function_args = line.split("(")[1].split(")")[0].split(",")
-                    # function_args = [i.strip() for i in function_args]
-                    # ofp.write(line)
-                    # line = fp.readline()
-                    # continue
-                # if "subroutine" in line.split("!")[0]:
-                if "subroutine" == exe_portion_list[0]:
-                    # subroutine_names.append(line.split("!")[0].split()[1].split("(")[0].strip())
-                    subroutine_names.append((exe_portion_list[1],file))
-                    # if len(line.split("(")) > 1:
-                    #     subroutine_args = line.split("(")[1].split(")")[0].split(",")
-                    #     subroutine_args = [i.strip() for i in subroutine_args]
-                    # ofp.write(line)
-                    # line = fp.readline()
-                    # continue
-                # if "module" == line.split("!")[0].split()[0].strip().lower():
-                if "module" == exe_portion_list[0]:
-                    # module_names.append(line.split("!")[0].split()[0].strip().lower())
-                    module_names.append((exe_portion_list[1], file))
-                    # module_names.append(line.split("!")[0].split()[1].split("(")[0].strip())
-                    # if len(line.split("(")) > 1:
-                    #     subroutine_args = line.split("(")[1].split(")")[0].split(",")
-                    #     subroutine_args = [i.strip() for i in subroutine_args]
-                    # ofp.write(line)
-                    # line = fp.readline()
-                    # continue
-                # skip_vars = function_names + function_args + subroutine_names + subroutine_args + \
-                #             intent_vars + external_func + fortran_intrinsics
+                token = line.split()[0].split("(")[0].lower()
+                if token in sub_tokens_beg: 
+                    in_sub = True
+                    sub_name = exe_portion_list[1]
+                    if sub_name.lower() != "date_and_time":
+                        subroutine_names.update({(sub_name,file):[]})
+                if token in sub_tokens_end: 
+                    in_sub = False
+                if in_sub:
+                    if token == "call":
+                        called_sub = exe_portion_list[1].lower()
+                        if called_sub != "date_and_time" and called_sub != "exit":
+                            if called_sub not in subroutine_names[(sub_name,file)]:
+                                subroutine_names[(sub_name,file)].append(called_sub)
 
             line = fp.readline()
                 
-print(subroutine_names)
-print()
-print()
-print(function_names)
-print()
-print()
-print(module_names)
-print()
-print()
+# with open("sub_dict.txt", "w") as sb:
+#     for x in subroutine_names.keys():
+#         print(x, subroutine_names[x])
+#         sb.write(str(x) + str(subroutine_names[x]) + "\n")
 
-# Second part of the code creates a dictionay representing the call tree.
+# Second part of the code creates an odered dictionay representing the call tree.
 # This dictionary will later be converted to .dot graph file.  
-# The dictionary structure is similar to the hierarchy of .dot file. 
-# The starting point is main.f90 in source path.
+def get_sub_file_name(sub_name):
+    for item in subroutine_names:
+        if item[0] == sub_name:
+            return item[1]
+    return
 
-def recurse_code(file, dot_string):
-    # global subroutine_names
-    # global function_names
-    with open(file, errors='ignore') as fp:
-        # first make a pass through the file to collect a subroutine calls
-        calls = []
-        for line in fp.readlines():
-            exe_portion = line.split("!")[0].strip().lower()
-            exe_portion_list = re.split(r"\W+", exe_portion.strip())
-            if "call" == exe_portion_list[0]:
-                calls.append(exe_portion_list[1])
-        # add calls to dot_string
-        i = 0
-        for call in calls:
-            i += 1
-            if i == 1:
-                dot_string += "{"
-            dot_string += " " + call + " "
-        dot_string += "}\n"
+ordered_sub_dict = OrderedDict()
+
+def reorder_sub_dict(sub,file):
+    sub_names = subroutine_names[(sub, file)]
+    ordered_sub_dict.update({(sub,file):sub_names})
+    for sub in sub_names:
+        file = get_sub_file_name(sub)
+        reorder_sub_dict(sub, file)
+    return
+
+def convert_ordered_dict_to_dot(sub, file, dot_string):
+    # advance to the start
+    start = False
+    for key in ordered_sub_dict.keys(): 
+        if key != (sub, file) and start is False:
+            continue
+        else:
+            start = True
+            dot_string += key[0] + " -- " + "{"
+            for sub in ordered_sub_dict[key]:
+                dot_string += " " + sub + " "
+            dot_string += "}\n"
     return dot_string
+            
+reorder_sub_dict("main", "src/main.f90")
 
+with open("ordered_dict.txt", "w") as od:
+    for x in ordered_sub_dict.keys():
+        print(x, ordered_sub_dict[x])
+        od.write(str(x) + str(ordered_sub_dict[x]) + "\n")
 
-dot_string = "graph {\nrankdir=LR\nmain -- "
-dot_string = recurse_code("src/main.f90", dot_string)
+dot_string = "graph {\nrankdir=LR\n"
+dot_string = convert_ordered_dict_to_dot("main", "src/main.f90", dot_string)
 dot_string += "}"
 print(dot_string)
 with open("swatplus_call_tree.dot", "w") as wp:
     wp.write(dot_string)
-                
 
 
 
